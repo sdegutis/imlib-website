69ff1685e3d3d0db82603c7d040fd848e87b294f
sdegutis <sbdegutis@gmail.com>
2024-08-16 18:41:51 -0500

    Reimporting old docs in the laziest way possible.

--- site/getting-started.html.tsx
+++ site/getting-started.html.tsx
@@ -1,7 +1,20 @@
 import { Page } from "./page.js";
+import mdfiles from './md/';
+import MarkdownIt from 'markdown-it';
+
+const md = new MarkdownIt({
+  typographer: true,
+  html: true,
+  linkify: true,
+  breaks: true
+});
+
+const markdown = mdfiles[0]!.content.toString('utf8');
+
 
 export default <Page which="Getting Started">
   <link rel='stylesheet' href='style.css' />
   <h3>Using imlib</h3>
   <p>Clone the <a href='https://github.com/sdegutis/imlib-template-minimal'>template repo</a>.</p>
+  {md.render(markdown)}
 </Page>;

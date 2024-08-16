1a3b53eb60faf406f92973bffa577a9ffaf3a72b
sdegutis <sbdegutis@gmail.com>
2024-08-16 17:25:42 -0500

    Navbar beginnings.

---
+++ site/page.tsx
@@ -0,0 +1,23 @@
+const pages = {
+  'About': '/index.html',
+  'Getting Started': '/getting-started.html',
+} as const;
+
+export function Page(attrs: { which: keyof typeof pages }, children: any) {
+  return <>
+    {`<!doctype html>`}
+    <html>
+      <head />
+      <body>
+        <nav>
+          <ul>
+            {Object.entries(pages).map(([title, href]) => <li>
+              <a href={href} class={title === attrs.which ? 'current' : ''}>{title}</a>
+            </li>)}
+          </ul>
+        </nav>
+        {children}
+      </body>
+    </html>
+  </>;
+}

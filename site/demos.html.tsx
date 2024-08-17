d58fea64ed7b064354408bc212a77ff1a0b565f6
sdegutis <sbdegutis@gmail.com>
2024-08-17 17:05:34 -0500

    Fix link.

--- site/demos.html.tsx
+++ site/demos.html.tsx
@@ -4,7 +4,7 @@ export default <Page which="Demos">
   <link rel='stylesheet' href='style.css' />
   <h3>Demos</h3>
   <ul>
-    <li><a href='https://sdegutis.github.io/imlib-template/'>todo list</a> (<a href="https://github.com/sdegutis/imlib-template">source</a>)</li>
+    <li><a href='https://vanillajsx.com/'>todo list</a> (<a href="https://github.com/sdegutis/vanillajsx.com/blob/main/site/samples/sample5.tsx">source</a>)</li>
     <li><a href='https://minigamemaker.com/'>minigamemaker.com</a> (<a href="https://github.com/sdegutis/mini-game-ide">source</a>)</li>
     <li><a href='https://immaculatalibrary.com/'>immaculatalibrary.com</a> (<a href="https://github.com/sdegutis/immaculatalibrary.com">source</a>)</li>
     <li><a href='https://www.samanimate.com/'>samanimate.com</a> (<a href="https://github.com/sdegutis/samanimate">source</a>)</li>

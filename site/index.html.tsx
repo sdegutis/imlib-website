0fb4aff3fb99fb351be1f6d476884b1baae27858
sdegutis <sbdegutis@gmail.com>
2024-08-17 07:44:35 -0500

    Gonna try to make this website today.

--- site/index.html.tsx
+++ site/index.html.tsx
@@ -3,9 +3,8 @@ import { Page } from "./page.js";
 export default <Page which="About">
   <link rel='stylesheet' href='style.css' />
   <h3>imlib</h3>
-  <p>Docs and better site coming soon.</p>
   <ul>
-    <li>Very simple TypeScript SSG</li>
+    <li>Simple TypeScript/JSX SSG</li>
     <li>Custom JSX implementation (can return DOM nodes, strings, etc)</li>
     <li>Extremely fast and efficient (processes 1500+ files in ~700ms)</li>
   </ul>
